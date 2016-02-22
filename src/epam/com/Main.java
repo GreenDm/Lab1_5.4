85a6748dbf4f371718f2c1d7cb5c3601e77d3df9
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 11:49:37 +0200

    Initial commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,20 @@
+package epam.com;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+
+        for(int x = 0;x<=300; x++){
+
+            if (x==11){
+                x=20;
+            }
+            System.out.println(x);
+
+        }
+    }
+}
